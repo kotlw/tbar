a874f9d26fb2442be22bcffa4ef7d53ecdffd781
Vladyslav Kotliar <kotliarvladyslav@gmail.com>
2023-07-26 23:33:12 +0100

    update build script

--- build.sh
+++ build.sh
@@ -5,12 +5,17 @@ declare -xr RED="\033[31m"
 declare -xr GREEN="\033[32m"
 declare -xr YELLOW="\033[93m"
 
-declare ARGS_CONFIG=""
+declare -xr GIT_URL="https://github.com/kotlw/tbar"
+declare -xr DIR="$HOME/.config/zellij/plugins/tbar"
+
+declare ARGS_CONFIG="compact-bar" # default config
+declare ARGS_DOWNLOAD=0
 
 parse_args() {
   while [ "$#" -gt 0 ]; do
     case "$1" in
       -c | --config) shift; ARGS_CONFIG=$1 ;;
+      -d | --download) ARGS_DOWNLOAD=1 ;;
     esac
     shift
   done
@@ -36,7 +41,7 @@ function execute_step() {
   local index=0
   local framesCount="${#frames[@]}"
 
-  eval $1 >${log_path} 2>&1 & pid=$!
+  $($1 >${log_path} 2>&1) & pid=$!
   
   tput civis # Hide the cursor
 
@@ -53,7 +58,7 @@ function execute_step() {
     echo -ne " ${GREEN}${check_symbol} $2\n"
   else
     echo -ne " ${RED}${x_symbol} $2\n"
-    cat ${log_path}
+    cat "${log_path}" | while read line; do echo "  ${line}"; done
     tput cnorm
     exit 1
   fi
@@ -69,34 +74,56 @@ print_logo() {
   echo -e "${GREEN} ....${NC}..............."
 }
 
-check_if_cargo_exists() {
+print_footer() {
+  echo -e "${NC} ..................."
+  echo -e " Path to the plugin binary: file:~/.config/zellij/plugins/tbar.wasm"
+}
+
+check_dependencies() {
   if ! command -v cargo &> /dev/null; then
-    echo "cargo could not be found"
-    exit
+    echo -ne "  ${RED}cargo could not be found\n"
+    exit 1
+  fi
+
+  if ! command -v git &> /dev/null; then
+    echo -ne "  ${RED}git could not be found\n"
+    exit 1
   fi
 }
 
 apply_config() {
-  local config_file="./configs/${ARGS_CONFIG}.rs";
+  local config_file="${DIR}/configs/${ARGS_CONFIG}.rs";
 
   if ! test -f "${config_file}"; then
     echo -e "  ${RED}${config_file} could not be found"
     exit 1
   fi
 
-  cp "${config_file}" "./src/config.rs"
+  cp "${config_file}" "${DIR}/src/config.rs"
 }
 
+move_artifact() {
+  cp "${DIR}/target/wasm32-wasi/debug/tbar.wasm" "${DIR}/../"
+}
+
+download() {
+  mkdir -p "${DIR}"
+  git clone "${GIT_URL}" "${DIR}"
+}
 
 main() {
   parse_args "$@"
   print_logo
-  check_if_cargo_exists
-  if [ ! -z "${ARGS_CONFIG}" ]; then execute_step "apply_config" "selecting config"; fi
-  
+
+  execute_step "check_dependencies" "check dependencies"
+  [[ "${ARGS_DOWNLOAD}" -eq 1 ]] && execute_step "download" "downloading"
+  cd "${DIR}"
+  execute_step "apply_config" "applying ${ARGS_CONFIG} config"
   execute_step "cargo build" "building"
-  cp ./target/wasm32-wasi/debug/tbar.wasm ./
+  execute_step "move_artifact" "moving artifact to the plugin root"
   execute_step "" "done" 
+  
+  print_footer
 }
 
 main "$@"
